4bd03936a4fe20f66b6db1d2070764e928d81787
Vandan Dev Baranwal <vandandevbaranwal@gmail.com>
2026-06-18 22:35:08 +0530

    Add kth largest element in array solution - heap

---
+++ heap/kth_largest_element_in_array.py
@@ -0,0 +1,74 @@
+# Pattern: QuickSelect (Median-of-Three Pivot)
+# Trigger: "kth largest" = need one position correct, not full sorting
+
+class Solution:
+
+    def partition(self, nums: List[int], left: int, right: int) -> int:
+
+        # choose a better pivot using median-of-three
+        mid = (left + right) >> 1
+        nums[mid], nums[left + 1] = nums[left + 1], nums[mid]
+
+        if nums[left] < nums[right]:
+            nums[left], nums[right] = nums[right], nums[left]
+
+        if nums[left + 1] < nums[right]:
+            nums[left + 1], nums[right] = nums[right], nums[left + 1]
+
+        if nums[left] < nums[left + 1]:
+            nums[left], nums[left + 1] = nums[left + 1], nums[left]
+
+        pivot = nums[left + 1]
+
+        i = left + 1
+        j = right
+
+        while True:
+
+            # find element <= pivot
+            while True:
+                i += 1
+                if not nums[i] > pivot:
+                    break
+
+            # find element >= pivot
+            while True:
+                j -= 1
+                if not nums[j] < pivot:
+                    break
+
+            if i > j:
+                break
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        # place pivot into final position
+        nums[left + 1], nums[j] = nums[j], nums[left + 1]
+
+        return j
+
+    def quickSelect(self, nums: List[int], k: int) -> int:
+
+        left = 0
+        right = len(nums) - 1
+
+        while True:
+
+            # small partition base case
+            if right <= left + 1:
+
+                if right == left + 1 and nums[right] > nums[left]:
+                    nums[left], nums[right] = nums[right], nums[left]
+
+                return nums[k]
+
+            j = self.partition(nums, left, right)
+
+            if j >= k:
+                right = j - 1
+
+            if j <= k:
+                left = j + 1
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.quickSelect(nums, k - 1)
